3e8ff5ecba4028c3342de8f5d4e707246fbebd7a
davimp3 <davi.nepomuceno@icloud.com>
2026-07-18 11:03:11 -0300

    FEATURE - Ajustes de Seguranças + Adição de botão para o WPP

--- script.js
+++ script.js
@@ -363,8 +363,8 @@
       }
     });
   }, {
-    threshold: 0.1,
-    rootMargin: '0px 0px -50px 0px'
+    threshold: 0.02,
+    rootMargin: '0px 0px 40px 0px'
   });
 
   scrollRevealElements.forEach(el => {
